08ac86337f0d79713acfb7fa61c950d1a54a7242
Benazir Sultana <benazirsultana1998@gmail.com>
2022-11-17 17:13:16 +0530

    initial commit

--- src/ComponentF.jsx
+++ src/ComponentF.jsx
@@ -1,30 +1,32 @@
-import React from "react"
-import { useContext } from "react/cjs/react.development"
-import { Codingcontext, Usercontext } from "./App"
-let user2 = useContext(Usercontext)
-let coding2 = useContext(Codingcontext)
-export const ComponentF=()=>{
-  return(
+import React from "react";
+import { useContext } from "react/cjs/react.development";
+import { Codingcontext, Usercontext } from "./App";
+
+export const ComponentF = () => {
+  let user2 = useContext(Usercontext);
+  let coding2 = useContext(Codingcontext);
+  return (
     <div>
       <Usercontext.Consumer>
-   {
-     user =>{
-       return (
-        
-           <Codingcontext.Consumer>
-{
-  coding =>{
-    return <div>User context is <span style={{color:"red"}}>{user}</span> likes <span style={{color:"red"}}>{coding}</span></div>
-  }
-}
-           </Codingcontext.Consumer>
-         
-       )
-     }
-   }
-    </Usercontext.Consumer>
-    
-    <h1>{user2}-{coding2}</h1>
+        {(user) => {
+          return (
+            <Codingcontext.Consumer>
+              {(coding) => {
+                return (
+                  <div>
+                    User context is <span style={{ color: "red" }}>{user}</span>{" "}
+                    likes <span style={{ color: "red" }}>{coding}</span>
+                  </div>
+                );
+              }}
+            </Codingcontext.Consumer>
+          );
+        }}
+      </Usercontext.Consumer>
+
+      <h1>
+        {user2}-{coding2}
+      </h1>
     </div>
-  )
-}
+  );
+};
